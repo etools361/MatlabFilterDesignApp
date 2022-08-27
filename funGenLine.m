288e8cc1442d68b9bd60c36382172edfef5305a3
baobaoluo <wowobaobaoluo@163.com>
2022-08-27 21:38:09 +0800

    Update funGenLine.m

--- funGenLine.m
+++ funGenLine.m
@@ -6,11 +6,11 @@
 function [x0,y0]=funGenLine(axPlot, x, y, r)
 hold(axPlot, 'on');
 if r == 0
-    plot(axPlot, x, y, '-k', 'LineWidth', 2);
+    plot(axPlot, x, y, '-k', 'LineWidth', 1);
     x0 = x(end);
     y0 = y(end);
 else
-    plot(axPlot, y, x, '-k', 'LineWidth', 2);
+    plot(axPlot, y, x, '-k', 'LineWidth', 1);
     x0 = y(end);
     y0 = x(end);
 end
