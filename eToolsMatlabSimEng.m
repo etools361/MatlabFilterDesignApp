042b297da16cff228c2147e54b8689ce69ced137
baobaoluo <wowobaobaoluo@163.com>
2022-08-27 21:37:47 +0800

    Update eToolsMatlabSimEng.m

--- eToolsMatlabSimEng.m
+++ eToolsMatlabSimEng.m
@@ -6,8 +6,8 @@
 % netlist, PI型，两端接载
 tic;
 fType  = 'Butterworth';
-fShape = 'HPF';
-n     = 11;
+fShape = 'LPF';
+n     = 6;
 Rs    = 1;
 Rl    = 1;
 fp    = 1;
@@ -16,6 +16,7 @@ Ap    = -3;
 As    = Ap-15;
 bw    = [];
 TeeEn = 1;% TeeEn=0:PI, TeeEn=1:Tee
+SimSW = 8;
 % 滤波器综合
 [strNetlist] = funSynthesisFilter(fType, TeeEn, n, Rs, Rl, fp, fs, Ap, As, bw, fShape);
 % 滤波器仿真
@@ -32,35 +33,53 @@ strDevice = 'RL'; % 显示结果的器件
 [MM, MN, MV, MX] = funSimNetlist2Matrix(iType, Value, node1, node2, maxNode, nL, nI, nV, nR0, cellName);
 
 %% -----------------Gen Sch-------------------------
-figure(1);
-axPlot = gca;
-f0     = 1e-3/2/pi;
-w0     = 2*pi*f0;
-[img] = funGenSchematic2(axPlot, iType, Value, cellNode1, CellNode2, cellName, w0);
-set(gcf,'color',[1,1,1]);
+if bitand(SimSW, 1)
+    figure(1);
+    axPlot = gca;
+    f0     = 1e-3/2/pi;
+    w0     = 2*pi*f0;
+    [img] = funGenSchematic2(axPlot, iType, Value, cellNode1, CellNode2, cellName, w0);
+    set(gcf,'color',[1,1,1]);
+end
 %% -----------------AC-------------------------
-f0 = 1e-2;
-f1 = 1e1;
-% f0 = 1e4;
-% f1 = 1e6;
-N     = 500;
-h2    = figure(2);
-axMag = axes(h2);
-h3    = figure(3);
-axPhase = axes(h3);
-funACSim(axMag, axPhase, f0, f1, N, cellName, MX, MM, MN, MV, Value, node1, node2);
-
+if bitand(SimSW, 2)
+    f0 = 1e-2;
+    f1 = 1e1;
+    % f0 = 1e4;
+    % f1 = 1e6;
+    N     = 500;
+    h2    = figure(2);
+    axMag = axes(h2);
+    h3    = figure(3);
+    axPhase = axes(h3);
+    funACSim(axMag, axPhase, f0, f1, N, cellName, MX, MM, MN, MV, Value, node1, node2);
+end
 %% -----------------Tran-------------------------
-f1     = 2e-2;
-% f1=1;
-% Tmax=30/f1;
-Tmax   = 2/f1;
-Nmax   = 10000; % 
-h4     = figure(4);
-axTran = axes(h4);
-oldXTick = findall(axTran,'Tag', 'pseudoXTickLabels'); 
-delete(oldXTick)
-funTranSim(axTran, f1, Tmax, Nmax, MX, MM, MN, MV, retNode, cellName, Value)
-toc;
-
+if bitand(SimSW, 4)
+    f1     = 2e-2;
+    
+    % f1=1;
+    % Tmax=30/f1;
+    Tmax   = 2/f1;
+    Nmax   = 10000; % 
+    h4     = figure(4);
+    axTran = axes(h4);
+    oldXTick = findall(axTran,'Tag', 'pseudoXTickLabels'); 
+    delete(oldXTick)
+    funTranSim(axTran, f1, Tmax, Nmax, MX, MM, MN, MV, retNode, cellName, Value);
+end
+%% -----------------PZ-------------------------
+if bitand(SimSW, 8)
+    f1       = 2;
+    f0       = 1e-2;
+    h5       = figure(5);
+    axPZ     = axes(h5);
+    h6       = figure(6);
+    axPZ3D   = axes(h6);
+    N        = 16*n;
+    Disp3DEn = 1;
+    A0       = 1/4*sqrt(Rs/Rl);
+    funPZSim(axPZ, axPZ3D, f0, f1, N, cellName, MX, MM, MN, MV, Value, node1, node2, A0, Ap);
+    toc;
+end
 
