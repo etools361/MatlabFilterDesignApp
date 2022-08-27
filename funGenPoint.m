10d864342a9cb788ffbe4e8602c0ff46b3134b15
baobaoluo <wowobaobaoluo@163.com>
2022-08-27 21:38:11 +0800

    Update funGenPoint.m

--- funGenPoint.m
+++ funGenPoint.m
@@ -6,8 +6,10 @@
 function [x, y]=funGenPoint(axPlot, x, y, r)
 hold(axPlot, 'on');
 if r == 0
-    plot(axPlot, x, y, '-ok', 'LineWidth', 6);
+    plot(axPlot, x, y, '-ok', 'LineWidth', 3);
+    plot(axPlot, x, y, '-*k', 'LineWidth', 2);
 else
-    plot(axPlot, y, x, '-ok', 'LineWidth', 6);
+    plot(axPlot, y, x, '-ok', 'LineWidth', 3);
+    plot(axPlot, y, x, '-*k', 'LineWidth', 2);
 end
 hold(axPlot, 'off');
