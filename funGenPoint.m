5d6ff33015d2605b2ab980bd138800c630be0d30
baobaoluo <wowobaobaoluo@163.com>
2022-08-28 22:12:45 +0800

    Update funGenPoint.m

--- funGenPoint.m
+++ funGenPoint.m
@@ -6,10 +6,8 @@
 function [x, y]=funGenPoint(axPlot, x, y, r)
 hold(axPlot, 'on');
 if r == 0
-    plot(axPlot, x, y, '-ok', 'LineWidth', 3);
-    plot(axPlot, x, y, '-*k', 'LineWidth', 2);
+    plot(axPlot, x, y, '-ok', 'LineWidth', 1, 'MarkerFaceColor','k');
 else
-    plot(axPlot, y, x, '-ok', 'LineWidth', 3);
-    plot(axPlot, y, x, '-*k', 'LineWidth', 2);
+    plot(axPlot, y, x, '-ok', 'LineWidth', 1, 'MarkerFaceColor','k');
 end
 hold(axPlot, 'off');
