023889fab43b4806eacfb4ac94b14bafbdad01d9
baobaoluo <wowobaobaoluo@163.com>
2022-09-21 01:37:22 +0800

    Update eToolsMatlabSimEng.m

--- eToolsMatlabSimEng.m
+++ eToolsMatlabSimEng.m
@@ -5,15 +5,15 @@
 %--------------------------------------------------------------------------
 % netlist, PI型，两端接载
 tic;
-fType  = 'Butterworth';
+fType  = 'Chebyshev I';%'Butterworth';
 fShape = 'LPF';
-n     = 3;
+n     = 7;
 Rs    = 1;
 Rl    = 2;
 fp    = 0.159;
 fs    = 2;
-Ap    = -3;
-As    = Ap-15;
+Ap    = 3;
+As    = -Ap-15;
 bw    = [];
 TeeEn = 1;% TeeEn=0:PI, TeeEn=1:Tee
 SimSW = 3;
@@ -21,9 +21,21 @@ SimSW = 3;
 [strNetlist] = funSynthesisFilter(fType, TeeEn, n, Rs, Rl, fp, fs, Ap, As, bw, fShape);
 % 滤波器仿真
 % split netlist
-[iType, Value, cellNode1, CellNode2, cellName] = funSimNetlist2Array(strNetlist);
+[iType, Value, CellNode1, CellNode2, cellName] = funSimNetlist2Array(strNetlist);
+% 实际器件替换
+[iType, Value] = funSynthesisFilterActual(iType, Value);
+[strNetlist] = funSimArray2Netlist(iType, Value, CellNode1, CellNode2, cellName);
+% strNetlist = {
+% 'V0 V 9 0 1.000000e+00';
+% 'RS R 1 9 1.000000e+00';
+% 'C1 C 1 0 2.26';
+% 'L2 L 1 2 1.51';
+% 'C3 C 2 0 1.51';
+% 'L4 L 2 3 2.26';
+% 'RL R 3 0 1.000000e+00';
+% };
 % netlist standard
-[node1, node2] = funSimNetlistRenode(cellNode1, CellNode2);
+[node1, node2] = funSimNetlistRenode(CellNode1, CellNode2);
 % netlist analysis
 [maxNode, nL, nI, nV, nR0] = funSimNetlistAna(iType, Value, node1, node2);
 % 标记给定器件的非GND节点用于获取结果位置
@@ -38,13 +50,13 @@ if bitand(SimSW, 1)
     axPlot = gca;
     f0     = 1e-3/2/pi;
     w0     = 2*pi*f0;
-    [img] = funGenSchematic2(axPlot, iType, Value, cellNode1, CellNode2, cellName, w0);
+    [img] = funGenSchematic2(axPlot, iType, Value, CellNode1, CellNode2, cellName, w0);
     set(gcf,'color',[1,1,1]);
 end
 %% -----------------AC-------------------------
 if bitand(SimSW, 2)
     f0 = 1e-2;
-    f1 = 1e1;
+    f1 = 1e0;
     % f0 = 1e4;
     % f1 = 1e6;
     N     = 500;
@@ -52,7 +64,8 @@ if bitand(SimSW, 2)
     axMag = axes(h2);
     h3    = figure(3);
     axPhase = axes(h3);
-    funACSim(axMag, axPhase, f0, f1, N, cellName, MX, MM, MN, MV, Value, node1, node2);
+    [IdealFreq, IdealMag, IdealPhase, P, Z] = funSimFilterIdeal(fType, TeeEn, n, Rs, Rl, fp, fs, Ap, As, bw, fShape, f0, f1, N);
+    funACSim(axMag, axPhase, f0, f1, N, cellName, MX, MM, MN, MV, Value, node1, node2, IdealFreq, IdealMag, IdealPhase);
 end
 %% -----------------Tran-------------------------
 if bitand(SimSW, 4)
@@ -78,8 +91,8 @@ if bitand(SimSW, 8)
     axPZ3D   = axes(h6);
     N        = 16*n;
     Disp3DEn = 1;
-    A0       = 1/4*sqrt(Rs/Rl);
-    funPZSim(axPZ, axPZ3D, f0, f1, N, cellName, MX, MM, MN, MV, Value, node1, node2, A0, Ap);
+    A0       = 10*log10(1/4*sqrt(Rs/Rl));
+    funPZSim(axPZ, axPZ3D, f0, f1, N, cellName, MX, MM, MN, MV, Value, node1, node2, A0, Ap, P, Z);
     toc;
 end
 
