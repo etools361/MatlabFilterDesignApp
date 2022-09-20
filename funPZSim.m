84883df2adac45d5df16eadd9440f43e356414de
baobaoluo <wowobaobaoluo@163.com>
2022-09-21 01:37:54 +0800

    Update funPZSim.m

--- funPZSim.m
+++ funPZSim.m
@@ -1,9 +1,9 @@
 %--------------------------------------------------------------------------
 % Edited by bbl
 % Date: 2022-08-18(yyyy-mm-dd)
-% AC仿真
+% PZ仿真
 %--------------------------------------------------------------------------
-function funPZSim(axPZ, axPZ3D, f0, f1, N, cellName, MX, MM, MN, MV, Value, node1, node2, A0, Ap)
+function funPZSim(axPZ, axPZ3D, f0, f1, N, cellName, MX, MM, MN, MV, Value, node1, node2, A0, Ap, P, Z)
 freq = linspace(-f1, f1, N);
 [FX, FY] = meshgrid(freq, freq);
 F        = FX+1i*FY;
@@ -32,50 +32,47 @@ for jj=1:N
     end
 end
 dBVo = 10*log10(abs(Vo2));
-% dBVo(dBVo<-80) = NaN;
-% fH = @(s) 1./(-s.^(2*n)+1);% 巴特沃斯滤波器
-% H  = fH(F).*A0;
-% Hi = 10*log10(abs(H));
-% if Disp3DEn
 axPZ1 = axPZ3D;
-    surfc(axPZ1, FX, FY, dBVo, 'LineWidth', 0.1);
-%     shading(axPZ1, 'interp');
-%     lighting(axPZ1, 'flat');
-%     light(axPZ1, 'position',[-1,0.5,1]*f1,'style','local','color','w')
-    hold(axPZ1, 'on');
-    % surf(axPZ, FX, FY, Hi);%ssss
-    b  = freq;
-    ibb = b>0;
-    bb = b(ibb);
-    plot3(axPZ1, bb.*0, bb, dBVo(ibb, fix(N/2)), '-*r', 'LineWidth', 2);
-    hold(axPZ1, 'off');
-
-    axis(axPZ1, 'tight');
-    view(axPZ1, [1 1 1]);%侧视
-    xlabel(axPZ1, 'real');
-    ylabel(axPZ1, 'image');
-    minZ = min(min(dBVo));
-    if minZ>-80
-        zlim(axPZ1, [minZ, A0+3]);
-    else
-        zlim(axPZ1, [-80, A0+3]);
+surfc(axPZ1, FX, FY, dBVo, 'LineWidth', 0.1);
+hold(axPZ1, 'on');
+b  = freq;
+ibb = b>0;
+bb = b(ibb);
+plot3(axPZ1, bb.*0, bb, dBVo(ibb, fix(N/2)), '-*r', 'LineWidth', 2);
+hold(axPZ1, 'off');
+axis(axPZ1, 'tight');
+view(axPZ1, [1 1 1]);%侧视
+xlabel(axPZ1, 'real');
+ylabel(axPZ1, 'image');
+minZ = round(min(min(dBVo)));
+maxZ = round(max(max(dBVo)));
+if minZ>-80 
+    if minZ<maxZ
+        zlim(axPZ1, [minZ, maxZ]);
     end
-    xlim(axPZ1, [freq(1), freq(end)]);
-    ylim(axPZ1, [freq(1), freq(end)]);
-    grid(axPZ1, 'on');
-    title(axPZ1, '3D PZ Plot');
-% else
-    contour(axPZ, FX, FY, dBVo', [minZ:(minZ-Ap),(A0+Ap):A0], 'LineWidth', 2);
-%     hold on;
-%     plot(axPZ, [1], [1], '*');
-%     hold off;
-    grid(axPZ,   'on');
-    axis(axPZ,   'square');
-    ylabel(axPZ, 'real');
-    xlabel(axPZ, 'image');
-    xlim(axPZ, [freq(1), freq(end)]);
-    ylim(axPZ, [freq(1), freq(end)]);
-    title(axPZ, 'PZ Plot');
-% end
+else
+    if maxZ>-80
+        zlim(axPZ1, [-80, maxZ]);
+    end
+end
+xlim(axPZ1, [freq(1), freq(end)]);
+ylim(axPZ1, [freq(1), freq(end)]);
+grid(axPZ1, 'on');
+title(axPZ1, '3D PZ Plot');
+LowSlice = linspace(minZ,(minZ+3), 2);
+HightSlice = linspace(maxZ-3, maxZ, 2);
+contour(axPZ, FX, FY, dBVo', [LowSlice,HightSlice], 'LineWidth', 2);
+hold(axPZ, 'on');
+plot(axPZ, imag(P), real(P), 'x');
+plot(axPZ, imag(Z), real(Z), 'o');
+% grid on;xlabel('real/\delta');ylabel('imag/jw');axis square;xlim([-1, 1]);ylim([-1,1])
+hold(axPZ, 'off');
+grid(axPZ,   'on');
+axis(axPZ,   'square');
+ylabel(axPZ, 'real');
+xlabel(axPZ, 'image');
+xlim(axPZ, [freq(1), freq(end)]);
+ylim(axPZ, [freq(1), freq(end)]);
+title(axPZ, 'PZ Plot');
 
 end
