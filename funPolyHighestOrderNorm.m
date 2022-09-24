344f7c8584d3f574bac7bdbb02f489fc2facb304
baobaoluo <wowobaobaoluo@163.com>
2022-09-25 02:37:49 +0800

    Update funPolyHighestOrderNorm.m

--- funPolyHighestOrderNorm.m
+++ funPolyHighestOrderNorm.m
@@ -7,14 +7,15 @@ function [P, Z] = funPolyHighestOrderNorm(P, Z)
 % 最高次项系数归一化
 nP = length(P);
 BreakEn = 0;
+Err = 1e-9;
 for ii=1:nP
-    if (Z(nP+1-ii)==1 && P(nP+1-ii)==0) || (Z(nP+1-ii)==0 && P(nP+1-ii)==1) || (Z(nP+1-ii)==1 && P(nP+1-ii)==1)
+    if (abs(Z(nP+1-ii)-1)<Err && abs(P(nP+1-ii))<Err) || (abs(Z(nP+1-ii))<Err && abs(P(nP+1-ii)-1)<Err) || (abs(Z(nP+1-ii)-1)<Err && abs(P(nP+1-ii)-1)<Err)
         break;
     end
-    if Z(nP+1-ii)~=1 && Z(nP+1-ii)~=0
+    if abs(Z(nP+1-ii)-1)>Err && abs(Z(nP+1-ii))>Err
         KH = Z(nP+1-ii);
         BreakEn = 1;
-    elseif P(nP+1-ii)~=1 && P(nP+1-ii)~=0
+    elseif abs(P(nP+1-ii)-1)>Err && abs(P(nP+1-ii))>Err
         KH = P(nP+1-ii);
         BreakEn = 1;
     else
